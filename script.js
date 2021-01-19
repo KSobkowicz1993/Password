005678108df522f051ffa175c3d3f6e6363ae394
Karolina Sobkowicz <karolina.klapyta@gmail.com>
2021-01-19 19:30:59 +0100

    first version

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict'
+
+$(function () {
+
+    const a = $('a');
+    let pass = $('#pass');
+    let numberOfSign = $('#znaki');
+    let counter = 0;
+
+    //liczy dobrze pod warunkiem ze wpisze sie haslo od razu bez uzycia backspace :(
+
+    pass.on('keydown', () => {
+
+        if (counter <= 16) {
+
+            console.log(counter);
+            numberOfSign.html(counter);
+
+            counter++;
+
+        } else {
+            alert('Dozwolona liczba znaków to 16!');
+        }
+
+    });
+
+    a.on('click', function (e) {
+
+        e.preventDefault();
+
+        pass.attr('type', 'text');
+
+        a.html('Ukryj znaki');
+
+    });
+
+});
